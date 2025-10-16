c3160866816723a1d3164a512b7716987ab37331
Tedi Setiandi <58543180+tedi25@users.noreply.github.com>
2025-10-16 19:09:19 +0700

    fix: delete invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -124,13 +124,7 @@ export async function updateInvoice(
 
 export async function deleteInvoice(id: string) {
   // throw new Error("Failed to Delete Invoice")
-  try {
-    await sql`DELETE FROM invoices WHERE id = ${id}`
-  } catch (error) {
-    console.error(error)
-    return {
-      message: "Database Error: Failed to Delete Invoice.",
-    }
-  }
+  // Unreachable code block
+  await sql`DELETE FROM invoices WHERE id = ${id}`
   revalidatePath("/dashboard/invoices")
 }
